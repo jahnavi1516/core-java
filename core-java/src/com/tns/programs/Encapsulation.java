b29a69d1e668c35f551e7f63255bd6ab330c3845
jahna <jahna@jahnavi>
2023-09-23 18:00:39 +0530

    first commit

---
+++ core-java/src/com/tns/programs/Encapsulation.java
@@ -0,0 +1,15 @@
+package com.tns.programs;
+// program to demostrate encapsulation in java
+public class Encapsulation {
+ // data members
+	public int serialno;
+     public String name;
+     public int age;
+   
+    //membersfunction
+    
+    public void show() {
+    	System.out.println("serialno"+ serialno+ "\t"+ "name"+ name+ "\t"+ "age"+ age);
+    }
+
+}
---
+++ core-java/src/com/tns/programs/EncapsulationDemo.java
@@ -0,0 +1,14 @@
+package com.tns.programs;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Encapsulation ob=new Encapsulation();//created object
+        ob.serialno=28;//accesing the datamember by using dot operator
+        ob.name="jahnavi";
+        ob.age=21;
+        ob.show();
+	}
+
+}
