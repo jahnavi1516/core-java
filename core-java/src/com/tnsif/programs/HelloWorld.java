faa5ad19a432d05407782166776c410037b1f29b
jahna <jahna@jahnavi.Dlink>
2023-09-25 14:21:54 +0530

    first commit

--- core-java/src/com/tnsif/programs/HelloWorld.java
+++ core-java/src/com/tnsif/programs/HelloWorld.java
@@ -1,10 +1,10 @@
 package com.tnsif.programs;
 
-public class Hello {
+public class HelloWorld{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("HelloWorld");
 	}
 
 }
